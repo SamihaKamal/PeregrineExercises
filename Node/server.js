277e51e94929ec15b18cb509f7c7468e3745b37c
SamihaKamal <samihakamal.7664@gmail.com>
2024-06-20 14:07:21 +0100

    node stuff for 1

---
+++ Node/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+var cors = require('cors');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/index.html', function(req,res) {
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/pagetwo.html', function(req,res) {
+    res.sendFile(__dirname + '/pagetwo.html');
+})
+app.get('/pagethree.html', function(req,res) {
+    res.sendFile(__dirname + '/pagethree.html');
+})
+
+var server = app.listen(3000, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("App is listening at http://%s:%s",host,port)
+});
