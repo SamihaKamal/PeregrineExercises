a72ff9e5c88a44322bb79ec4cb7bf5aa5ea417c1
SamihaKamal <samihakamal.7664@gmail.com>
2024-07-01 11:18:58 +0100

    did angular tasks

---
+++ Angular/AngularAppMVC/AngularAppMVC/Scripts/scriptDDL.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('ScriptDDL', []);
+app.controller('MyController', function ($scope) {
+    $scope.MyVal = "Welcome welcome!";
+
+    $scope.MyFun = function () {
+        alert("Hello world! ");
+    }
+    $scope.MyFun2 = function (val) {
+        alert("Para: " + val);
+    }
+});
