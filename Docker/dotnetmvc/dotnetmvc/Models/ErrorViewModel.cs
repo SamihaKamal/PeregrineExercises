3fc3654295700f54db4c06a739b5a71d9817a6cc
SamihaKamal <samihakamal.7664@gmail.com>
2024-07-03 15:22:06 +0100

    did docker stuff

---
+++ Docker/dotnetmvc/dotnetmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
